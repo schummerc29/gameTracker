f4d7113f1f8e2d837eb0de26e88a31c4221af630
CJ Schummer <116977275+schummerc29@users.noreply.github.com>
2024-02-12 18:11:49 -0600

    Add files via upload
    Added ability to show full sqlite table and take input to insert new entries into said table. Working on resolution to prevent duplicate inserts into table next version

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # Declare all imports
-from functions import game_input
 import json
-from os import path
-import pandas as pd 
 import sqlite3
+import time
+from os import path
+from functions import game_input
+#import pandas as pd 
+
+
+con = sqlite3.connect("games.db")
+cur = con.cursor()
+print("Checking database for table...")
+time.sleep(2)
+try:
+    cur.execute("CREATE TABLE library(Game, System, Start, End, Rate)")
+except sqlite3.OperationalError:
+
+    print("Library table already exists")
 
 
 # Declare list and dictionary
@@ -14,7 +26,7 @@ ask = ''
 
 
 # Prompts for game info, adds to library then clears dictionary
-while True:
+while True:    
     game_input(game)
     library.append(game)
     with open(filename, 'w') as json_file:
@@ -36,10 +48,45 @@ with open (filename) as f:
     print("JSON Data unloaded: ")
     print(data)
 
-
 # Iterates through list of dictionaries and prints them out
 print("   ")
 print("   ")
-print("Game Data Added: ")
+print("Adding data to library: ")
 for x in library:
     print(x)
+
+# Parameterize and insert correct values in library table
+for x in library:
+    for key, value in x.items():
+        game = x["Game"]
+        system = x["System"]
+        start = x["Start Date"]
+        end = x["Completed / Abandoned Date"]
+        rate = x["Rating"]
+        cur.execute('''
+          INSERT INTO library (Game, System, Start, End, Rate)
+            VALUES (?, ?, ?, ?, ?)''', (game, system, start, end, rate))
+# Adding each game five times....need to resolve
+        
+
+
+# Fetch All distinct Rows in Library table
+statement = '''SELECT DISTINCT * FROM library'''
+cur.execute(statement)
+
+print("Reading table data:")
+output = cur.fetchall()
+for row in output:
+    print(row)
+con.commit()
+
+
+# Close connection to DB
+print("Closing connection...")
+try:
+    con.close()
+    time.sleep(2)
+except sqlite3.OperationalError as e:
+    print(e)
+else:
+    print("Connection closed")
